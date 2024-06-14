9cb690690eb5fc3a7d74bcafab8521fe6764cf8b
Ansh Dixit <awesomeansh03@gmail.com>
2024-06-14 19:20:03 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/speech-recognition-app",
-  define: {
-    "process.env": {},
+  base: "/",
+  build: {
+    outDir: 'dist',
   },
 })
